8e61c45fc0416e7405f00465810321aa761edc92
Roland Schemers <schemers@stanford.edu>
2002-11-01 23:39:34 +0000

    document

--- src/webkdc/WebKDC/Exception.pm
+++ src/webkdc/WebKDC/Exception.pm
@@ -79,3 +79,117 @@ sub match {
 
 
 1;
+
+__END__
+
+=head1 NAME
+
+WebKDC::Exception - exceptions for WebKDC
+
+=head1 SYNOPSIS
+
+  use WebKDC;
+  use WebKDC::Exception;
+
+  eval {  
+    $data = WebKDC::base64_decode($buffer);
+    ...
+  };
+  if (WebKDC::Exception::match($@)) {
+    my $e = $@;
+    # you can call the following methods on an Exception object:
+    # $e->status()
+    # $e->message()
+    # $e->wrapped_exception()
+    # $e->verbose_message()
+  }
+
+=head1 DESCRIPTION
+
+The various WebKDC functions can all throw exceptions if something
+wrong happens. Some of these exceptions will be of type WebKDC::Exception,
+while some may be uncaught WebAuth::Exceptions.
+
+=head1 EXPORT
+
+The following constants are exported:
+
+  WK_ERR_USER_AND_PASS_REQUIRED
+  WK_ERR_LOGIN_FAILED
+  WK_ERR_UNRECOVERABLE_ERROR
+
+=over 4
+
+=item WK_ERR_USER_AND_PASS_REQUIRED
+
+ This status code indicates that a function was called
+ that required a username and password. The user should
+ be prompted for their username and the function should be
+ called again.
+
+=item WK_ERR_LOGIN_FAILED
+
+ This status code indicates that a function was called
+ that attempted to validate the username and password and
+ could not, due to an invalid user or password. The
+ user should be re-prompted for their username/password
+ and the function should be called again.
+
+=item WK_ERR_UNRECOVERABLE_ERROR
+
+ This status code indicates that a function was called
+ and an error occured that can not be recovered from. If
+ you are in the process of attempting to log a user in,
+ you have no choice but to display an error message to
+ the user and not prompt again.
+
+=back
+
+=head1 METHODS and FUNCTIONS
+
+=over 4
+
+=item match($exception[, $status])
+
+  This class function (not a method) returns true if the given
+  $exception is a WebAuth::Exception. If $status is specified, then
+  $exception->status() will also be compared to $status.
+
+=item new(status, message, wrapped_exception)
+
+  This method is used to created new WebKDC::Exception objects.
+
+=item status()
+
+  This method returns the WebKDC::Exception status code for the exception,
+  which will be one of the WK_ERR_* codes.
+
+=item message()
+
+  This method returns the WebKDC::Exception error message that was
+  used in the constructor.
+
+=item wrapped_exceptione()
+
+  This method returns the wrapped exception that was used in the
+  constructor (if there was one).
+
+=item verbose_message()
+
+  This method returns a verbose error message, which consists
+  of the status code, message, and any wrapped exception.
+
+  The verbose_message method is also called if the exception is
+  used as a string.
+
+=back
+
+=head1 AUTHOR
+
+Roland Schemers (schemers@stanford.edu)
+
+=head1 SEE ALSO
+
+L<WebKDC>.
+
+=cut
