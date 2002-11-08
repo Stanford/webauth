1e71c76262753db1e8323d7df6b5e340fce5f730
Roland Schemers <schemers@stanford.edu>
2002-11-08 22:44:57 +0000

    remove debug

--- src/webkdc/WebKDC/WebKDCException.pm
+++ src/webkdc/WebKDC/WebKDCException.pm
@@ -76,9 +76,7 @@ sub to_string {
 sub match {
     my $e = shift;
     return 0 if !isa($e, "WebKDC::WebKDCException");
-    return 1 unless @_;
-    return $e->status() == shift;
-    #return @_ ? $e->status() == shift : 1;
+    return @_ ? $e->status() == shift : 1;
 }
 
 
