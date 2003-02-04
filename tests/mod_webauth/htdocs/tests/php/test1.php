5845a5317de2f3cc84f9d12a35421c7ec8c73579
Roland Schemers <schemers@stanford.edu>
2003-02-04 01:01:28 +0000

    use heredoc

--- tests/mod_webauth/htdocs/tests/php/test1.php
+++ tests/mod_webauth/htdocs/tests/php/test1.php
@@ -1,46 +1,40 @@
 <?php 
 
-print "<html>";
-print "<title>PHP test1</title>";
-print "<h2>PHP test1: test from PHP module</h2>";
+$ru_env = getenv('REMOTE_USER');
 
-print "<hr>";
-print 'all the webauth variables can be found in $_SERVER or by calling ';
-print 'getenv<br><br>';
+print <<<EOS
+<html>
+<title>PHP test1</title>
+<h2>PHP test1: test from PHP module</h2>
+<hr>
+all the webauth variables can be found in \$_SERVER or by calling
+getenv<br><br>
 
-print '<table border="1" cellpadding="3" >';
-print '<tr align=center>';
-print '  <th>Name</th>';
-print '  <th>Value</th>';
-print '</tr>';
+<table border="1" cellpadding="3" >
+ <tr align=center>
+  <th>Name</th>
+  <th>Value</th>
+ </tr>
+ <tr><td>\$_SERVER["AUTH_TYPE"]</td>  <td>{$_SERVER['AUTH_TYPE']}</td></tr>
+ <tr><td>\$_SERVER["REMOTE_USER"]</td> <td>{$_SERVER['REMOTE_USER']}</td></tr>
+ <tr><td>\$_SERVER["WEBAUTH_USER"]</td><td>{$_SERVER['WEBAUTH_USER']}</td></tr>
+  <tr><td>\$_SERVER["WEBAUTH_TOKEN_CREATION"]</td>
+          <td>{$_SERVER['WEBAUTH_TOKEN_CREATION']}</td></tr>
+  <tr><td>\$_SERVER["WEBAUTH_TOKEN_EXPIRATION"]</td>
+           <td>{$_SERVER['WEBAUTH_TOKEN_EXPIRATION']}</td></tr>
+  <tr><td>getenv("REMOTE_USER")</td> <td>$ru_env</td></tr>
+</table>
+<br>
+<hr>
+<br>
+Click <a href="/tests/index.html">here</a> to return without logging out<br>
+Click <a href="/tests/auth/logout">here</a> to logout and return<br>
+<br>
+<hr>
+<br>
 
-print '<tr><td>$_SERVER["AUTH_TYPE"]</td>';
-print "<td>".$_SERVER['AUTH_TYPE']."</td></tr>";
+EOS;
 
-print '<tr><td>$_SERVER["REMOTE_USER"]</td>';
-print "<td>".$_SERVER['REMOTE_USER']."</td></tr>";
-
-print '<tr><td>$_SERVER["WEBAUTH_USER"]</td>';
-print "<td>".$_SERVER['WEBAUTH_USER']."</td></tr>";
-
-print '<tr><td>$_SERVER["WEBAUTH_TOKEN_CREATION"]</td>';
-print "<td>".$_SERVER['WEBAUTH_TOKEN_CREATION']."</td></tr>";
-
-print '<tr><td>$_SERVER["WEBAUTH_TOKEN_EXPIRATION"]</td>';
-print "<td>".$_SERVER['WEBAUTH_TOKEN_EXPIRATION']."</td></tr>";
-
-print '<tr><td>getenv("REMOTE_USER")</td>';
-print "<td>".getenv('REMOTE_USER')."</td></tr>";
-
-print "</table>";
-print "<br>";
-print "<hr>";
-print "<br>";
-print 'Click <a href="/tests/index.html">here</a> to return without logging out<br>';
-print 'Click <a href="/tests/auth/logout">here</a> to logout and return<br>';
-print "<br>";
-print "<hr>";
-print "<br>";
 phpinfo(); 
 
 print "</html>";
