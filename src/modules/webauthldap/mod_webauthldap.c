32de001a240ecc02b5a5d72e694091fff432b65f
Anton Ushakov <antonu@stanford.edu>
2003-04-24 22:37:47 +0000

    on second thought, we'll just log a NOTICE in the normal fashion upon startup.

--- src/modules/webauthldap/mod_webauthldap.c
+++ src/modules/webauthldap/mod_webauthldap.c
@@ -354,11 +354,9 @@ static int
 post_config_hook(apr_pool_t *pconf, apr_pool_t *plog,
                  apr_pool_t *ptemp, server_rec *s) 
 {
-    char* version;
     MWAL_SCONF *sconf = (MWAL_SCONF*)ap_get_module_config(s->module_config,
                                                           &webauthldap_module);
-    if (sconf->debug) ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, s, 
-                                   "webauthldap: initializing");
+
     // These all must be non-null:
 #define NULCHECK(val, label) \
     if (val == NULL) die_directive(s, label, ptemp);
@@ -386,8 +384,7 @@ post_config_hook(apr_pool_t *pconf, apr_pool_t *plog,
         sconf->ldarray = apr_array_make(pconf, 10, sizeof(LDAP *));
     }
 
-    version = apr_pstrcat(ptemp, "WebAuthLdap", NULL);
-    ap_add_version_component(pconf, version);
+    ap_log_error(APLOG_MARK, APLOG_NOTICE, 0, s, "WebAuthLdap: initialized");
 
     return OK;
 }
