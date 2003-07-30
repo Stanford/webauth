07b7b6ad24ede274309ae52b0f8f86231a608baa
Roland Schemers <schemers@stanford.edu>
2003-07-30 00:02:21 +0000

    bunch-o-windows stuff

--- windows/buildenv.bat
+++ windows/buildenv.bat
@@ -2,11 +2,12 @@
 rem these paths should point to the actual build directories of the
 rem following products, not any sort of "install" tree
 
-set APACHE_PATH=c:\cygwin\home\schemers\src\httpd-2.0.46
-set OPENSSL_PATH=c:\cygwin\home\schemers\src\httpd-2.0.46\srclib\openssl
-rem set OPENSSL_PATH=c:\cygwin\home\schemers\src\openssl-0.9.7b
-set OPENLDAP_PATH=c:\cygwin\home\schemers\src\openldap-2.1.20
-set SASL_PATH=c:\cygwin\home\schemers\src\cyrus-sasl-2.1.13
-set KRB5_PATH=c:\cygwin\home\schemers\src\krb5-1.2.8
-set CURL_PATH=c:\cygwin\home\schemers\src\curl-7.10.4
-
+set BUILD=c:\cygwin\home\schemers\vc6
+set APACHE=c:\Program Files\Apache Group\Apache2
+set APACHE_PATH=%BUILD%\httpd-2.0.47
+set OPENSSL_PATH=%BUILD%\httpd-2.0.47\srclib\openssl
+set OPENLDAP_PATH=%BUILD%\openldap-2.1.22
+set SASL_PATH=%BUILD%\cyrus-sasl-2.1.15
+set KRB5_PATH=%BUILD%\krb5-1.2.8
+set CURL_PATH=%BUILD%\curl-7.10.6
+set WEBAUTH_PATH=%BUILD%\webauth3
