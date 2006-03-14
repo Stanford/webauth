9719b7b3ad0e7442ea9257c9a255d57a2894cff0
Russ Allbery <rra@stanford.edu>
2006-03-14 19:13:55 +0000

    Prototype all of the possible variables so that people don't need to put the "our" in front of the variable in webkdc.conf.

--- src/webkdc/WebKDC/Config.pm
+++ src/webkdc/WebKDC/Config.pm
@@ -10,6 +10,8 @@ our $TEMPLATE_PATH = "./generic/templates";
 our $URL = "https://localhost/webkdc-service/";
 our $HONOR_REMOTE_USER = 0;
 our $REMUSER_EXPIRES = 60 * 60 * 8;
+our $REALM;
+our $REMOTE_USER_REDIRECT;
 
 if (-f $conf) {
     my $ret = do $conf;
