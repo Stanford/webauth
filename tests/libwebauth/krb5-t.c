819a4d5d0dabf412fb37d5244c31dc76f599024e
Russ Allbery <rra@stanford.edu>
2010-06-17 14:26:04 -0700

    Convert the Kerberos tests to use libtap and run by default
    Convert the extra libwebauth Kerberos tests to use libtap and my
    normal test configuration system and run them by default.  Add
    documentation for how to configure the Kerberos tests.
    
    Running these tests properly required importing more of the test
    framework from rra-c-util, including the portability layer and some
    portions of libutil.  These are not yet used through the rest of
    the code but will be in the future.
    
    Temporarily add some additional probes to m4/krb5.m4.  This file
    will be replaced by the version from rra-c-util in the near future.

--- tests/libwebauth/krb5-t.c
+++ tests/libwebauth/krb5-t.c
@@ -2,184 +2,179 @@
  * Test suite for libwebauth Kerberos functions.
  *
  * Written by Roland Schemers
- * Copyright 2002, 2003, 2006, 2008, 2009
+ * Updated for current TAP library support by Russ Allbery
+ * Copyright 2002, 2003, 2006, 2008, 2009, 2010
  *     Board of Trustees, Leland Stanford Jr. University
  *
  * See LICENSE for licensing terms.
  */
 
-#include "config.h"
+#include <config.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include <lib/webauth.h>
-#include "webauthtest.h"
+#include <tests/tap/basic.h>
 
 #define BUFSIZE 4096
 #define MAX_ATTRS 128
 
-void
-usage()
-{
-  printf("usage: krb5_test {username} {password} {keytab} {service} {host}\n");
-  printf("  keytab         name of keytab file used to verify tgt\n");
-  printf("  service/host   name of service/host to test export_ticket with\n");
-  exit(1);
-}
 
-int main(int argc, char *argv[])
+int
+main(void)
 {
     int s;
     WEBAUTH_KRB5_CTXT *c;
-    TEST_VARS;
-    char *username, *password, *keytab_path, *server, *server_principal;
-    char *service, *host;
+    char username[BUFSIZ], password[BUFSIZ], principal[BUFSIZ];
+    char *server_principal, *service, *host, *keytab, *path, *server, *p;
     char *cp;
     char *sa;
     int salen;
     char *tgt, *ticket;
     int tgtlen, ticketlen;
     time_t expiration;
-    char *cprinc, *crealm;
+    char *cprinc = NULL;
+    char *crealm = NULL;
+    FILE *file;
 
-    if (argc != 6) {
-        usage();
+    /* Read the configuration information. */
+    path = test_file_path("data/test.principal");
+    if (path == NULL)
+        skip_all("Kerberos tests not configured");
+    keytab = test_file_path("data/test.keytab");
+    if (keytab == NULL)
+        skip_all("Kerberos tests not configured");
+    file = fopen(path, "r");
+    if (file == NULL)
+        sysbail("cannot open %s", path);
+    if (fgets(principal, sizeof(principal), file) == NULL) {
+        fclose(file);
+        bail("cannot read %s", path);
     }
-
-    username = argv[1];
-    password = argv[2];
-    keytab_path = argv[3];
-    service = argv[4];
-    host = argv[5];
-
-    cprinc = NULL;
-    crealm = NULL;
-
-    START_TESTS(32);
+    fclose(file);
+    if (principal[strlen(principal) - 1] != '\n')
+        bail("no newline in %s", path);
+    test_file_path_free(path);
+    principal[strlen(principal) - 1] = '\0';
+    path = test_file_path("data/test.password");
+    if (path == NULL)
+        skip_all("Kerberos tests not configured");
+    file = fopen(path, "r");
+    if (file == NULL)
+        sysbail("cannot open %s", path);
+    if (fgets(username, sizeof(username), file) == NULL)
+        bail("cannot read %s", path);
+    if (fgets(password, sizeof(password), file) == NULL)
+        bail("cannot read password from %s", path);
+    fclose(file);
+    if (username[strlen(username) - 1] != '\n')
+        bail("no newline in %s", path);
+    username[strlen(username) - 1] = '\0';
+    if (password[strlen(password) - 1] != '\n')
+        bail("username or password too long in %s", path);
+    password[strlen(password) - 1] = '\0';
+    test_file_path_free(path);
+    
+    plan(30);
 
     s = webauth_krb5_new(&c);
-    TEST_OK2(WA_ERR_NONE, s);
-    TEST_OK(c != NULL);
+    is_int(WA_ERR_NONE, s, "Creating a context succeeds");
+    ok(c != NULL, "...and the context is not NULL");
 
-    /* test failure case */
+    /* We can't get information before we initialize. */
     s = webauth_krb5_get_principal(c, &cprinc, 1);
-    TEST_OK2(WA_ERR_INVALID_CONTEXT, s);
+    is_int(WA_ERR_INVALID_CONTEXT, s,
+           "Getting the principal fails with the right error");
     s = webauth_krb5_get_realm(c, &crealm);
-    TEST_OK2(WA_ERR_INVALID_CONTEXT, s);
+    is_int(WA_ERR_INVALID_CONTEXT, s,
+           "Getting the realm fails with the right error");
 
-    s = webauth_krb5_init_via_password(c, username, password, NULL,
-                                       keytab_path, NULL,
-                                       NULL, &server_principal);
+    /* Do the authentication with the username and password. */
+    s = webauth_krb5_init_via_password(c, username, password, NULL, keytab,
+                                       NULL, NULL, &server_principal);
+    is_int(WA_ERR_NONE, s, "Kerberos initialization succeeds");
+    ok(server_principal != NULL, "...and returns the server principal");
 
-    TEST_OK2(WA_ERR_NONE, s);
-    TEST_OK(server_principal != NULL);
-
+    /* Test principal canonicalization. */
     s = webauth_krb5_get_principal(c, &cprinc, WA_KRB5_CANON_LOCAL);
-    /*printf("cprinc = %s\n", cprinc);*/
-    TEST_OK2(WA_ERR_NONE, s);
-    TEST_OK(strchr(cprinc, '@') == NULL);
+    is_int(WA_ERR_NONE, s, "Local canonicalization");
     free(cprinc);
     s = webauth_krb5_get_principal(c, &cprinc, WA_KRB5_CANON_NONE);
-    TEST_OK2(WA_ERR_NONE, s);
-    TEST_OK(strchr(cprinc, '@') != NULL);
+    is_int(WA_ERR_NONE, s, "No canonicalization");
+    ok(strchr(cprinc, '@') != NULL, "...and resulting principal has a realm");
     free(cprinc);
     s = webauth_krb5_get_principal(c, &cprinc, WA_KRB5_CANON_STRIP);
-    TEST_OK2(WA_ERR_NONE, s);
-    TEST_OK(strchr(cprinc, '@') == NULL);
-
+    is_int(WA_ERR_NONE, s, "Strip canonicalization");
+    ok(strchr(cprinc, '@') == NULL, "...and resulting principal has no realm");
     s = webauth_krb5_get_realm(c, &crealm);
-    /*printf("crealm = %s\n", crealm);*/
-    TEST_OK2(WA_ERR_NONE, s);
-
-    /*
-    printf("code(%d) mess(%s)\n",
-           webauth_krb5_error_code(c),
-           webauth_krb5_error_message(c));
-    */
+    is_int(WA_ERR_NONE, s, "Getting the realm");
 
     sa = NULL;
-
     s = webauth_krb5_mk_req(c, server_principal, &sa, &salen);
+    is_int(WA_ERR_NONE, s, "Building an AP-REQ");
     free(server_principal);
-
-    TEST_OK2(WA_ERR_NONE, s);
-
-    s = webauth_krb5_rd_req(c, sa, salen, keytab_path, NULL, &cp, 1);
-    /*printf("cp = %s\n", cp);*/
-    TEST_OK2(WA_ERR_NONE, s);
-    if (cp) {
+    s = webauth_krb5_rd_req(c, sa, salen, keytab, NULL, &cp, 1);
+    is_int(WA_ERR_NONE, s, "...and it then validates");
+    if (cp != NULL)
         free(cp);
-    }
-
-    if (sa != NULL) {
+    if (sa != NULL)
         free(sa);
-    }
 
     tgt = NULL;
     s = webauth_krb5_export_tgt(c, &tgt, &tgtlen, &expiration);
-    TEST_OK2(WA_ERR_NONE, s);
-
-    s = webauth_krb5_service_principal(c, service, host, &server);
-    TEST_OK2(WA_ERR_NONE, s);
-
-    s = webauth_krb5_export_ticket(c, server,
-                                   &ticket, &ticketlen, &expiration);
-    free(server);
-
-    TEST_OK2(WA_ERR_NONE, s);
+    is_int(WA_ERR_NONE, s, "Exporting a TGT");
+    s = webauth_krb5_export_ticket(c, principal, &ticket, &ticketlen,
+                                   &expiration);
+    is_int(WA_ERR_NONE, s, "Exporting the service ticket");
 
     s = webauth_krb5_free(c);
-    TEST_OK2(WA_ERR_NONE, s);
+    is_int(WA_ERR_NONE, s, "Freeing the context");
 
-    if (tgt != NULL) {
+    if (tgt == NULL)
+        skip_block(4, "TGT creation failed");
+    else {
         s = webauth_krb5_new(&c);
-        TEST_OK2(WA_ERR_NONE, s);
-        TEST_OK(c != NULL);
-
+        is_int(WA_ERR_NONE, s, "Creating a new context");
+        ok(c != NULL, "...and the context is not NULL");
         s = webauth_krb5_init_via_cred(c, tgt, tgtlen, NULL);
+        is_int(WA_ERR_NONE, s, "Initializing with a credential");
         free(tgt);
-        TEST_OK2(WA_ERR_NONE, s);
-
-        if (ticket != NULL) {
+        if (ticket == NULL)
+            skip("Service ticket exporting failed");
+        else {
             s = webauth_krb5_import_cred(c, ticket, ticketlen);
-            /*free(ticket);*/
-            TEST_OK2(WA_ERR_NONE, s);
+            is_int(WA_ERR_NONE, s, "Importing a ticket");
         }
-
         s = webauth_krb5_free(c);
-        TEST_OK2(WA_ERR_NONE, s);
+        is_int(WA_ERR_NONE, s, "Freeing the context");
     }
 
-    if (tgt != NULL) {
+    if (ticket == NULL)
+        skip_block(4, "Ticket exporting failed");
+    else {
         s = webauth_krb5_new(&c);
-        TEST_OK2(WA_ERR_NONE, s);
-        TEST_OK(c != NULL);
-
+        is_int(WA_ERR_NONE, s, "Creating a new context");
+        ok(c != NULL, "...and the context is not NULL");
         s = webauth_krb5_init_via_cred(c, ticket, ticketlen, NULL);
+        is_int(WA_ERR_NONE, s, "Initializing with a ticket");
         free(ticket);
-        TEST_OK2(WA_ERR_NONE, s);
-
-        /*s = webauth_krb5_keep_cred_cache(c);*/
-
         s = webauth_krb5_free(c);
-        TEST_OK2(WA_ERR_NONE, s);
+        is_int(WA_ERR_NONE, s, "Freeing the context");
     }
 
     s = webauth_krb5_new(&c);
-    TEST_OK2(WA_ERR_NONE, s);
-    TEST_OK(c != NULL);
-
-    s = webauth_krb5_init_via_keytab(c, keytab_path, NULL, NULL);
-    TEST_OK2(WA_ERR_NONE, s);
-
+    is_int(WA_ERR_NONE, s, "Creating a new context");
+    ok(c != NULL, "...and the context is not NULL");
+    s = webauth_krb5_init_via_keytab(c, keytab, NULL, NULL);
+    is_int(WA_ERR_NONE, s, "Initializing with a keytab");
     s = webauth_krb5_free(c);
-    TEST_OK2(WA_ERR_NONE, s);
+    is_int(WA_ERR_NONE, s, "Freeing the context");
 
     if (cprinc != NULL)
         free(cprinc);
 
-    END_TESTS;
-    exit(NUM_FAILED_TESTS ? 1 : 0);
+    return 0;
 }
--- tests/libwebauth/krb5-tgt-t.c
+++ tests/libwebauth/krb5-tgt-t.c
@@ -2,64 +2,44 @@
  * Test suite for libwebauth Kerberos TGT manipulation.
  *
  * Written by Roland Schemers
- * Copyright 2003, 2006, 2009
+ * Updated for current TAP library support by Russ Allbery
+ * Copyright 2003, 2006, 2009, 2010
  *     Board of Trustees, Leland Stanford Jr. University
  *
  * See LICENSE for licensing terms.
  */
 
-#include "config.h"
+#include <config.h>
 
 #include <stdio.h>
-
-#ifdef HAVE_STDLIB_H
 #include <stdlib.h>
-#endif
-
-#ifdef HAVE_STRING_H
 #include <string.h>
-#endif
+#include <unistd.h>
 
 #include <lib/webauth.h>
-#include "webauthtest.h"
+#include <tests/tap/basic.h>
+#include <tests/tap/kerberos.h>
 
-#define BUFSIZE 4096
-#define MAX_ATTRS 128
+#define CHECK(s, c, m) check_status(s, c, m, __FILE__, __LINE__)
+
 
 void
-usage(char *prog)
+check_status(int s, WEBAUTH_KRB5_CTXT *c, const char *message,
+             const char *file, int line)
 {
-  printf("usage: %s {command}\n", prog);
-  printf("  export {principal} [cache]    export tgt to specified princpal\n");
-  printf("  import {keytab} {cache} {req} {tgt}   import an exported tgt\n");
-  exit(1);
+    if (s == WA_ERR_KRB5 && c != NULL)
+        diag("webauth call failed %s line %d: %s (%d): %s %d\n", file, line,
+             webauth_error_message(s), s, webauth_krb5_error_message(c),
+             webauth_krb5_error_code(c));
+    else if (s != WA_ERR_NONE)
+        diag("webauth call failed %s line %d: %s (%d)\n", file, line,
+             webauth_error_message(s), s);
+    is_int(s, WA_ERR_NONE, "%s", message);
 }
 
-void check_status(int s, WEBAUTH_KRB5_CTXT *c, char *file, int line)
-{
-    if (s == WA_ERR_NONE)
-        return;
-
-
-    if (s == WA_ERR_KRB5 && c != NULL) {
-        fprintf(stderr, 
-                "webauth call failed %s line %d: %s (%d): %s %d\n",
-                file, line,
-                webauth_error_message(s), s,
-                webauth_krb5_error_message(c),
-                webauth_krb5_error_code(c));
-    } else {
-        fprintf(stderr, 
-                "webauth call failed %s line %d: %s (%d)\n",
-                file, line,
-                webauth_error_message(s), s);
-    }
-    exit(1);
-}
-
-#define CHECK(s, c) check_status(s, c, __FILE__, __LINE__)
    
-void do_export(char *principal, char *cache)
+void
+do_export(const char *principal, const char *cache)
 {
     int s;
     WEBAUTH_KRB5_CTXT *c;
@@ -68,87 +48,107 @@ void do_export(char *principal, char *cache)
     time_t expiration;
     char b64[4192];
     int b64_len;
+    FILE *export;
 
+    export = fopen("test-cred", "w");
+    if (export == NULL)
+        sysbail("cannot create test-cred");
     s = webauth_krb5_new(&c);
-    CHECK(s,c);
+    CHECK(s, c, "Create context");
     s = webauth_krb5_init_via_cache(c, cache);
-    CHECK(s,c);
+    CHECK(s, c, "Initialize from cache");
     s = webauth_krb5_keep_cred_cache(c);
-    CHECK(s,c);
+    CHECK(s, c, "Mark cache as kept");
     s = webauth_krb5_export_tgt(c, &tgt, &tgt_len, &expiration);
-    CHECK(s,c);
-    s = webauth_krb5_mk_req_with_data(c, principal,
-                                      &req, &req_len,
-                                      tgt, tgt_len,
-                                      &etgt, &etgt_len);
-    CHECK(s,c);
-
-    s = webauth_base64_encode(req, req_len, b64, &b64_len, sizeof(b64)-1);
-    CHECK(s,c);
+    CHECK(s, c, "Export TGT");
+    s = webauth_krb5_mk_req_with_data(c, principal, &req, &req_len,
+                                      tgt, tgt_len, &etgt, &etgt_len);
+    CHECK(s, c, "Make AP-REQ with data");
+    s = webauth_base64_encode(req, req_len, b64, &b64_len, sizeof(b64) - 1);
+    CHECK(s, c, "base64-encode AP-REQ");
     b64[b64_len] = '\0';
-    printf("REQ(%d) = %s\n", b64_len, b64);
-
-    s = webauth_base64_encode(etgt, etgt_len, b64, &b64_len, sizeof(b64)-1);
-    CHECK(s,c);
+    fprintf(export, "%s\n", b64);
+    s = webauth_base64_encode(etgt, etgt_len, b64, &b64_len, sizeof(b64) - 1);
+    CHECK(s, c, "base64-encode supporting data");
     b64[b64_len] = '\0';
-    printf("ETGT(%d) = %s\n", b64_len, b64);
+    fprintf(export, "%s\n", b64);
+    fclose(export);
 
     free(tgt);
     free(req);
     free(etgt);
     s = webauth_krb5_free(c);
-    CHECK(s,c);
+    CHECK(s, c, "Free context");
 }
 
-void do_import(char *keytab, char *cache, char *req, char *tgt)
+
+void
+do_import(const char *keytab)
 {
     int s;
     WEBAUTH_KRB5_CTXT *c;
+    char req[4192], tgt[4192];
     int req_len, tgt_len, dec_tgt_len;
     char *dec_tgt;
     char *cprinc;
     char *sprinc;
+    FILE *data;
+
+    data = fopen("test-cred", "r");
+    if (data == NULL)
+        sysbail("cannot open test-cred");
+    if (fgets(req, sizeof(req), data) == NULL)
+        sysbail("error reading from test-cred");
+    if (req[strlen(req) - 1] != '\n')
+        bail("newline not found in test-cred");
+    req[strlen(req) - 1] = '\0';
+    if (fgets(tgt, sizeof(tgt), data) == NULL)
+        sysbail("error reading from test-cred");
+    if (tgt[strlen(tgt) - 1] != '\n')
+        bail("newline not found in test-cred");
+    tgt[strlen(tgt) - 1] = '\0';
+    fclose(data);
+    unlink("test-cred");
 
     s = webauth_base64_decode(req, strlen(req), req, &req_len, strlen(req));
-    CHECK(s, NULL);
-
+    CHECK(s, NULL, "base64-decode AP-REQ");
     s = webauth_base64_decode(tgt, strlen(tgt), tgt, &tgt_len, strlen(tgt));
-    CHECK(s, NULL);
-
+    CHECK(s, NULL, "base64-decode additional data");
     s = webauth_krb5_new(&c);
-    CHECK(s,c);
+    CHECK(s, c, "Create new context");
     s = webauth_krb5_rd_req_with_data(c, req, req_len, keytab, NULL, &sprinc,
                                       &cprinc, 1, tgt, tgt_len, &dec_tgt,
                                       &dec_tgt_len);
-    CHECK(s,c);
-    printf("cprinc = %s\n", cprinc);
-    printf("cprinc = %s\n", sprinc);
+    CHECK(s, c, "Read request and data");
+    ok(cprinc != NULL, "Client principal is not NULL");
+    ok(sprinc != NULL, "Server principal is not NULL");
     free(cprinc);
     free(sprinc);
-
-    s = webauth_krb5_init_via_cred(c, 
-                                   dec_tgt, 
-                                   dec_tgt_len,
-                                   (cache != NULL && *cache) ? cache : NULL);
-    CHECK(s,c);
+    s = webauth_krb5_init_via_cred(c, dec_tgt, dec_tgt_len, NULL);
+    CHECK(s, c, "Initialize from credentials");
     s = webauth_krb5_keep_cred_cache(c);
-    CHECK(s,c);
+    CHECK(s, c, "Save credential cache");
     s = webauth_krb5_free(c);
-    CHECK(s,NULL);
+    CHECK(s, NULL, "Free context");
 }
 
 
 int 
-main(int argc, char *argv[])
+main(void)
 {
-    if (argc < 3 || argc > 6)
-        usage(argv[0]);
+    char *principal, *keytab;
 
-    if ((strcmp(argv[1], "export") == 0) && (argc == 3 || argc == 4))
-        do_export(argv[2], argv[3]);
-    else if ((strcmp(argv[1], "import") == 0) && argc == 6)
-        do_import(argv[2], argv[3], argv[4], argv[5]);
-    else 
-        usage(argv[0]);
-    exit(0);
+    /* Read the configuration information. */
+    principal = kerberos_setup();
+    if (principal == NULL)
+        skip_all("No valid Kerberos ticket cache");
+    keytab = test_file_path("data/test.keytab");
+
+    /* Do the tests. */
+    plan(17);
+    do_export(principal, getenv("KRB5CCNAME"));
+    do_import(keytab);
+
+    kerberos_cleanup();
+    return 0;
 }
