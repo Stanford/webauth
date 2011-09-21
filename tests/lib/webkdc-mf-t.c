e2440f2bc750a93205e95b8894db00837cf67796
Russ Allbery <rra@stanford.edu>
2011-09-21 14:29:22 -0700

    Import new Kerberos TAP module from rra-c-util 3.9
    Use an atexit handler to clean up after Kerberos tests.
    
    Change-Id: Ib187161004baf111f0f7e2304b231869bfbf3529
    Reviewed-on: https://gerrit.stanford.edu/207
    Tested-by: Russ Allbery <rra@stanford.edu>
    Reviewed-by: Russ Allbery <rra@stanford.edu>

--- tests/lib/webkdc-mf-t.c
+++ tests/lib/webkdc-mf-t.c
@@ -696,9 +696,7 @@ main(void)
     webauth_keyring_free(ring);
     webauth_key_free(session_key);
     test_file_path_free((char *) config.keytab_path);
-    free((char *) config.principal);
     remctld_stop(remctld);
-    kerberos_cleanup();
     apr_terminate();
     return 0;
 }
