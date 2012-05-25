e97c4895cdc2eb22b43ec579467f3146d2e33d27
Russ Allbery <rra@stanford.edu>
2012-05-24 23:41:59 -0700

    Add tests for remaining keyring functions except auto_update
    Change-Id: Ida7b42748df895fe6b50233a2da44176a5c1f726
    Reviewed-on: https://gerrit.stanford.edu/374
    Reviewed-by: Russ Allbery <rra@stanford.edu>
    Tested-by: Russ Allbery <rra@stanford.edu>

--- tests/lib/keyring-t.c
+++ tests/lib/keyring-t.c
@@ -27,15 +27,15 @@ main(void)
 {
     struct webauth_context *ctx;
     struct webauth_key *key;
-    struct webauth_keyring *ring;
-    struct webauth_keyring *ring2;
+    const struct webauth_key *best;
+    struct webauth_keyring *ring, *ring2;
     struct webauth_keyring_entry *entry;
     char *tmpdir, *keyring;
     int s, fd;
     size_t i;
     time_t now;
 
-    plan(34);
+    plan(58);
 
     if (webauth_context_init(&ctx, NULL) != WA_ERR_NONE)
         bail("cannot initialize WebAuth context");
@@ -48,12 +48,12 @@ main(void)
     is_int(1, ring->entries->nelts, "... with one entry");
     entry = &APR_ARRAY_IDX(ring->entries, 0, struct webauth_keyring_entry);
     is_int(0, entry->creation, "Key has 0 creation time");
-    is_int(0, entry->valid_after, "...and 0 valid after");
+    is_int(0, entry->valid_after, "... and 0 valid after");
     ok(entry->key != key, "Key in the ring is a copy");
-    is_int(key->type, entry->key->type, "...with correct type");
-    is_int(key->length, entry->key->length, "...and length");
+    is_int(key->type, entry->key->type, "... with correct type");
+    is_int(key->length, entry->key->length, "... and length");
     ok(memcmp(key->data, entry->key->data, key->length) == 0,
-       "...and correct data");
+       "... and correct data");
 
     /* Create a ring with a specific capacity and add a couple of keys. */
     ring = webauth_keyring_new(ctx, 1);
@@ -66,14 +66,14 @@ main(void)
     is_int(1, ring->entries->nelts, "... with one entry");
     entry = &APR_ARRAY_IDX(ring->entries, 0, struct webauth_keyring_entry);
     is_int(now, entry->creation, "Key has correct creation time");
-    is_int(now, entry->valid_after, "...and correct valid after");
+    is_int(now, entry->valid_after, "... and correct valid after");
     s = webauth_key_create(ctx, WA_KEY_AES, WA_AES_128, NULL, &key);
     is_int(WA_ERR_NONE, s, "Creating another new random key succeeds");
     webauth_keyring_add(ctx, ring, now, now + 3600, key);
     is_int(2, ring->entries->nelts, "Keyring now has two entries");
     entry = &APR_ARRAY_IDX(ring->entries, 1, struct webauth_keyring_entry);
     is_int(now, entry->creation, "Second key has correct creation time");
-    is_int(now + 3600, entry->valid_after, "...and correct valid after");
+    is_int(now + 3600, entry->valid_after, "... and correct valid after");
 
     /* Write the keyring out and then read it back in. */
     tmpdir = test_tmpdir();
@@ -83,7 +83,7 @@ main(void)
     s = webauth_keyring_read(ctx, keyring, &ring2);
     is_int(WA_ERR_NONE, s, "Reading the keyring back from a file succeeds");
     is_int(ring->entries->nelts, ring2->entries->nelts,
-           "...and the key count matches");
+           "... and the key count matches");
     for (i = 0; i < (size_t) ring->entries->nelts; i++) {
         struct webauth_keyring_entry *e1, *e2;
 
@@ -102,6 +102,64 @@ main(void)
     }
     s = webauth_keyring_write(ctx, ring2, keyring);
     is_int(WA_ERR_NONE, s, "Writing the second keyring back out succeeds");
+
+    /* Test removal of keys from a keyring. */
+    s = webauth_keyring_remove(ctx, ring, 2);
+    is_int(WA_ERR_NOT_FOUND, s,
+           "Removal of nonexistent key returns correct error");
+    s = webauth_keyring_remove(ctx, ring, 0);
+    is_int(WA_ERR_NONE, s, "Removing the first key returns success");
+    is_int(1, ring->entries->nelts, "Keyring now has one entry");
+    entry = &APR_ARRAY_IDX(ring->entries, 1, struct webauth_keyring_entry);
+    is_int(now, entry->creation, "First key has correct creation time");
+    is_int(now + 3600, entry->valid_after, "... and correct valid after");
+
+    /* Add keys back in. */
+    s = webauth_key_create(ctx, WA_KEY_AES, WA_AES_256, NULL, &key);
+    is_int(WA_ERR_NONE, s, "Creating a second key succeeds");
+    webauth_keyring_add(ctx, ring, now, now, key);
+    is_int(2, ring->entries->nelts, "Adding a key back in succeeds");
+    s = webauth_key_create(ctx, WA_KEY_AES, WA_AES_192, NULL, &key);
+    webauth_keyring_add(ctx, ring, now - 3600, now - 3600, key);
+    is_int(WA_ERR_NONE, s, "Creating a third key succeeds");
+    is_int(3, ring->entries->nelts,
+           "Adding the same key with different times succeeds");
+
+    /* Test finding the best decryption key. */
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_DECRYPT, now - 3600, &best);
+    is_int(WA_ERR_NONE, s, "Finding past decryption key works");
+    entry = &APR_ARRAY_IDX(ring->entries, 2, struct webauth_keyring_entry);
+    ok(entry->key == best, "... and matches the last key we added");
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_DECRYPT, now - 3601, &best);
+    is_int(WA_ERR_NOT_FOUND, s,
+           "... but a hint older than all valid after returns error");
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_DECRYPT, now - 1, &best);
+    is_int(WA_ERR_NONE, s,
+           "Finding a decryption key one second in the past works");
+    ok(entry->key == best, "... and matches the last key we added");
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_DECRYPT, now, &best);
+    is_int(WA_ERR_NONE, s, "Finding the current key succeeds");
+    is_int(WA_AES_256, best->length, "... and finds the 256-bit key");
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_DECRYPT, now + 3600, &best);
+    is_int(WA_ERR_NONE, s, "Finding a future key succeeds");
+    is_int(WA_AES_256, best->length, "... and still finds the 256-bit key");
+
+    /* Test finding the best encryption key. */
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_ENCRYPT, now, &best);
+    is_int(WA_ERR_NONE, s, "Finding an encryption key succeeds");
+    is_int(WA_AES_256, best->length, "... and finds the 256-bit key");
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_ENCRYPT, now - 3700, &best);
+    is_int(WA_ERR_NONE, s, "Finding a past encryption key succeeds");
+    is_int(WA_AES_256, best->length, "... and still finds the 256-bit key");
+
+    /* Test finding keys in an empty keyring. */
+    webauth_keyring_remove(ctx, ring, 2);
+    webauth_keyring_remove(ctx, ring, 1);
+    webauth_keyring_remove(ctx, ring, 0);
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_DECRYPT, now, &best);
+    is_int(WA_ERR_NOT_FOUND, s, "No decrypt key found in empty keyring");
+    s = webauth_keyring_best_key(ctx, ring, WA_KEY_ENCRYPT, now, &best);
+    is_int(WA_ERR_NOT_FOUND, s, "No encrypt key found in empty keyring");
 
     /* Truncate a keyring and test empty keyrings. */
     fd = open(keyring, O_WRONLY | O_TRUNC, 0644);
