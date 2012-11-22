7430108794b076f2f7bb5f36405f2929dba9df82
Russ Allbery <rra@stanford.edu>
2012-11-22 15:19:39 -0800

    Avoid the legacy attribute code when encoding
    Accumulate the attribute encoded form directly in an output buffer
    rather than using the attribute list and encoding code.  This is
    the first step towards eliminating the legacy, non-APR attribute
    handling code.
    
    Change-Id: Id8b8d67fbfbab7eae8f79c60dceaf83133e8fd8a
    Reviewed-on: https://gerrit.stanford.edu/575
    Reviewed-by: Russ Allbery <rra@stanford.edu>
    Tested-by: Russ Allbery <rra@stanford.edu>

--- lib/attr-encode.c
+++ lib/attr-encode.c
@@ -6,9 +6,6 @@
  * for some other WebAuth persistant data structures, such as service token
  * caches and keyrings.
  *
- * Currently, this still uses the WebAuth attribute list code beneath, but
- * eventually will do attribute encoding directly.
- *
  * Written by Russ Allbery <rra@stanford.edu>
  * Copyright 2012
  *     The Board of Trustees of the Leland Stanford Junior University
@@ -20,6 +17,7 @@
 #include <portable/apr.h>
 #include <portable/system.h>
 
+#include <netinet/in.h>
 #include <time.h>
 
 #include <lib/internal.h>
@@ -60,26 +58,88 @@ encode_error_set(struct webauth_context *ctx, int status, const char *desc,
 
 
 /*
- * Given an encoding specification, a data source, and an attribute list,
- * encode into attributes.  Takes a separate pool to use rather than using the
- * normal WebAuth context pool, since attribute encoding can churn a lot of
- * memory.  Context is a string to prepend to the description for error
- * reporting.  If context is non-NULL, we are handling a repeated attribute
- * encoding, and the element number is appended to the attribute name when
- * encoding it.
+ * Append an attribute and value to the output buffer.  Takes the output
+ * buffer, the attribute key (as a string), the value and its length, and a
+ * flag indicating whether to hex-encode the data.  Returns a WebAuth error
+ * code.
+ */
+static int
+encode_data(struct buffer *output, const char *attr, const void *data,
+            size_t length, bool ascii)
+{
+    size_t hexlen, i, enclen;
+    const char *in = data;
+    char *p;
+    int status;
+
+    wai_buffer_append_sprintf(output, "%s=", attr);
+    if (ascii) {
+        hexlen = webauth_hex_encoded_length(length);
+        wai_buffer_resize(output, output->used + hexlen + 1);
+        status = webauth_hex_encode(data, length, output->data + output->used,
+                                    &hexlen, hexlen);
+        if (status != WA_ERR_NONE)
+            return status;
+        output->used += hexlen;
+        output->data[output->used] = ';';
+        output->used++;
+    } else {
+        for (i = 0, enclen = 0; i < length; i++, enclen++)
+            if (in[i] == ';')
+                enclen++;
+        wai_buffer_resize(output, output->used + enclen + 1);
+        for (i = 0, p = output->data + output->used; i < length; i++, p++) {
+            *p = in[i];
+            if (in[i] == ';') {
+                p++;
+                *p = ';';
+            }
+        }
+        *p = ';';
+        output->used += enclen + 1;
+    }
+    return WA_ERR_NONE;
+}
+
+
+/*
+ * Encode an attribute and numeric value to the output buffer.  Takes the
+ * output buffer, the attribute key (as a string), the value as an unsigned
+ * integer, and a flag indicating whether to format the number as a string.
+ */
+static void
+encode_number(struct buffer *output, const char *attr, unsigned long value,
+              bool ascii)
+{
+    if (ascii)
+        wai_buffer_append_sprintf(output, "%s=%lu;", attr, value);
+    else {
+        uint32_t data = value;
+
+        data = htonl(data);
+        encode_data(output, attr, &data, sizeof(data), false);
+    }
+}
+
+
+/*
+ * Given an encoding specification, a data source, and a buffer, encode into
+ * attribute form in the given buffer.  Context is a string to prepend to the
+ * description for error reporting.  If context is non-NULL, we are handling a
+ * repeated attribute encoding, and the element number is appended to the
+ * attribute name when encoding it.
  *
  * This is an internal helper function used by wai_encode.
  */
 static int
-encode_to_attrs(struct webauth_context *ctx, apr_pool_t *pool,
-                const struct wai_encoding *rules, const void *input,
-                WEBAUTH_ATTR_LIST *alist, const char *context,
+encode_to_attrs(struct webauth_context *ctx, const struct wai_encoding *rules,
+                const void *input, struct buffer *output, const char *context,
                 unsigned long element)
 {
     const struct wai_encoding *rule;
     const char *attr;
     unsigned long i;
-    int status, flags;
+    int status;
     void *data, *repeat;
     int32_t int32;
     char *string;
@@ -92,7 +152,8 @@ encode_to_attrs(struct webauth_context *ctx, apr_pool_t *pool,
         if (context == NULL)
             attr = rule->attr;
         else
-            attr = apr_psprintf(pool, "%s%lu", rule->attr, element);
+            attr = apr_psprintf(ctx->pool, "%s%lu", rule->attr, element);
+        status = WA_ERR_NONE;
         switch (rule->type) {
         case WA_TYPE_DATA:
             data = *LOC_DATA(input, rule->offset);
@@ -103,8 +164,7 @@ encode_to_attrs(struct webauth_context *ctx, apr_pool_t *pool,
                 break;
             }
             size = *LOC_SIZE(input, rule->len_offset);
-            flags = rule->ascii ? WA_F_FMT_HEX : WA_F_NONE;
-            status = webauth_attr_list_add(alist, attr, data, size, flags);
+            status = encode_data(output, attr, data, size, rule->ascii);
             break;
         case WA_TYPE_STRING:
             string = *LOC_STRING(input, rule->offset);
@@ -114,29 +174,25 @@ encode_to_attrs(struct webauth_context *ctx, apr_pool_t *pool,
                 status = WA_ERR_INVALID;
                 break;
             }
-            status = webauth_attr_list_add_str(alist, attr, string,
-                                               strlen(string), WA_F_NONE);
+            status = encode_data(output, attr, string, strlen(string), false);
             break;
         case WA_TYPE_INT32:
             int32 = *LOC_INT32(input, rule->offset);
             if (rule->optional && int32 == 0)
                 break;
-            flags = rule->ascii ? WA_F_FMT_STR : WA_F_NONE;
-            status = webauth_attr_list_add_int32(alist, attr, int32, flags);
+            encode_number(output, attr, int32, rule->ascii);
             break;
         case WA_TYPE_UINT32:
             uint32 = *LOC_UINT32(input, rule->offset);
             if (rule->optional && uint32 == 0)
                 break;
-            flags = rule->ascii ? WA_F_FMT_STR : WA_F_NONE;
-            status = webauth_attr_list_add_uint32(alist, attr, uint32, flags);
+            encode_number(output, attr, uint32, rule->ascii);
             break;
         case WA_TYPE_ULONG:
             ulong = *LOC_ULONG(input, rule->offset);
             if (rule->optional && ulong == 0)
                 break;
-            flags = rule->ascii ? WA_F_FMT_STR : WA_F_NONE;
-            status = webauth_attr_list_add_uint32(alist, attr, ulong, flags);
+            encode_number(output, attr, ulong, rule->ascii);
             break;
         case WA_TYPE_TIME:
             timev = *LOC_TIME(input, rule->offset);
@@ -144,21 +200,17 @@ encode_to_attrs(struct webauth_context *ctx, apr_pool_t *pool,
                 timev = time(NULL);
             if (rule->optional && timev == 0)
                 break;
-            flags = rule->ascii ? WA_F_FMT_STR : WA_F_NONE;
-            status = webauth_attr_list_add_time(alist, attr, timev, flags);
+            encode_number(output, attr, timev, rule->ascii);
             break;
         case WA_TYPE_REPEAT:
             uint32 = *LOC_UINT32(input, rule->len_offset);
             if (rule->optional && uint32 == 0)
                 break;
-            flags = rule->ascii ? WA_F_FMT_STR : WA_F_NONE;
-            status = webauth_attr_list_add_uint32(alist, attr, uint32, flags);
-            if (status != WA_ERR_NONE)
-                break;
+            encode_number(output, attr, uint32, rules->ascii);
             for (i = 0; i < uint32; i++) {
                 repeat = *LOC_STRING(input, rule->offset) + rule->size * i;
-                status = encode_to_attrs(ctx, pool, rule->repeat, repeat,
-                                         alist, attr, i);
+                status = encode_to_attrs(ctx, rule->repeat, repeat, output,
+                                         attr, i);
                 if (status != WA_ERR_NONE)
                     return status;
             }
@@ -178,36 +230,22 @@ encode_to_attrs(struct webauth_context *ctx, apr_pool_t *pool,
  * into attributes and return the encoded string in newly-allocated pool
  * memory.  Takes a separate pool to use rather than using the normal WebAuth
  * context pool, since attribute encoding can churn a lot of memory.
- *
- * FIXME: This currently still uses the underlying attribute code, but should
- * be split off into its own implementation and made independent of that.
  */
 int
 wai_encode(struct webauth_context *ctx, apr_pool_t *pool,
            const struct wai_encoding *rules, const void *data, void **output,
            size_t *length)
 {
-    WEBAUTH_ATTR_LIST *alist;
+    struct buffer *buffer;
     int status;
-    size_t size;
 
-    alist = webauth_attr_list_new(32);
-    if (alist == NULL) {
-        wai_error_set(ctx, WA_ERR_NO_MEM, "creating attribute list");
-        return WA_ERR_NO_MEM;
-    }
-    status = encode_to_attrs(ctx, pool, rules, data, alist, NULL, 0);
+    buffer = wai_buffer_new(pool);
+    status = encode_to_attrs(ctx, rules, data, buffer, NULL, 0);
     if (status != WA_ERR_NONE)
-        goto done;
-    size = webauth_attrs_encoded_length(alist);
-    *output = apr_palloc(pool, size);
-    status = webauth_attrs_encode(alist, *output, length, size);
-    if (status != WA_ERR_NONE)
-        wai_error_set(ctx, status, "encoding attributes");
-
-done:
-    webauth_attr_list_free(alist);
-    return status;
+        return status;
+    *output = buffer->data;
+    *length = buffer->used;
+    return WA_ERR_NONE;
 }
 
 
@@ -221,33 +259,22 @@ wai_encode_token(struct webauth_context *ctx,
                  const struct webauth_token *token, void **output,
                  size_t *length)
 {
-    WEBAUTH_ATTR_LIST *alist;
+    struct buffer *buffer;
     int status;
-    size_t size;
+    const char *type;
     const struct wai_encoding *rules;
     const void *data;
 
     status = wai_token_encoding(ctx, token, &rules, &data);
     if (status != WA_ERR_NONE)
         return status;
-    alist = webauth_attr_list_new(10);
-    if (alist == NULL) {
-        wai_error_set(ctx, WA_ERR_NO_MEM, "creating attribute list");
-        return WA_ERR_NO_MEM;
-    }
-    webauth_attr_list_add_str(alist, "t",
-                              webauth_token_type_string(token->type), 0,
-                              WA_F_NONE);
-    status = encode_to_attrs(ctx, ctx->pool, rules, data, alist, NULL, 0);
+    buffer = wai_buffer_new(ctx->pool);
+    type = webauth_token_type_string(token->type);
+    wai_buffer_append_sprintf(buffer, "t=%s;", type);
+    status = encode_to_attrs(ctx, rules, data, buffer, NULL, 0);
     if (status != WA_ERR_NONE)
-        goto done;
-    size = webauth_attrs_encoded_length(alist);
-    *output = apr_palloc(ctx->pool, size);
-    status = webauth_attrs_encode(alist, *output, length, size);
-    if (status != WA_ERR_NONE)
-        wai_error_set(ctx, status, "encoding attributes");
-
-done:
-    webauth_attr_list_free(alist);
-    return status;
+        return status;
+    *output = buffer->data;
+    *length = buffer->used;
+    return WA_ERR_NONE;
 }
