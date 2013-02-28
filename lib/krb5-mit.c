287cec03ed01a3cc63872fe2abe1be2a5db21043
Russ Allbery <rra@stanford.edu>
2013-02-27 20:26:35 -0800

    Properly check the result when decoding Kerberos credentials
    We were ignoring the result of the decoding and continuing on with
    an all-zero credential struct.  We would reject that later for other
    reasons, but return the error right away.
    
    Reviewed-on: https://gerrit.stanford.edu/864
    Reviewed-by: Russ Allbery <rra@stanford.edu>
    Tested-by: Russ Allbery <rra@stanford.edu>
    (cherry picked from commit 76e87f9c9a5fa66d99985c1ea6248503f1cba016)
    
    Change-Id: I894cf19f0622976f44686fa59ff998e993efab0e
    Reviewed-on: https://gerrit.stanford.edu/900
    Reviewed-by: Russ Allbery <rra@stanford.edu>
    Tested-by: Russ Allbery <rra@stanford.edu>

--- lib/krb5-mit.c
+++ lib/krb5-mit.c
@@ -7,7 +7,7 @@
  *
  * Written by Russ Allbery <rra@stanford.edu>
  * Based on the original Kerberos support code by Roland Schemers
- * Copyright 2002, 2003, 2006, 2009, 2010, 2012
+ * Copyright 2002, 2003, 2006, 2009, 2010, 2012, 2013
  *     The Board of Trustees of the Leland Stanford Junior University
  *
  * See LICENSE for licensing terms.
@@ -134,6 +134,8 @@ decode_creds(struct webauth_context *ctx, struct webauth_krb5 *kc,
      */
     memset(&data, 0, sizeof(data));
     status = wai_decode(ctx, wai_krb5_cred_encoding, input, length, &data);
+    if (status != WA_ERR_NONE)
+        return status;
     memset(creds, 0, sizeof(krb5_creds));
     if (data.client_principal != NULL) {
         status = decode_principal(ctx, kc, data.client_principal,
